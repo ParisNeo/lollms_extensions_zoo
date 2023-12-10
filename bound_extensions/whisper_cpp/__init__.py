0de6d1784abdf73deff0e392c18252fefde8dd0f
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-12-10 14:48:51 +0100

    enhanced

--- bound_extensions/whisper_cpp/__init__.py
+++ bound_extensions/whisper_cpp/__init__.py
@@ -2,6 +2,7 @@ from lollms.extension import LOLLMSExtension
 from lollms.helpers import ASCIIColors
 from lollms.config import InstallOption, TypedConfig, BaseConfig, ConfigTemplate
 from lollms.utilities import PackageManager
+from lollms.media import AudioRecorder
 import subprocess
 from pathlib import Path
 import time
@@ -23,68 +24,6 @@ else:
 import threading
 
 extension_name="Whisper"
-
-
-class AudioRecorder:
-    def __init__(self, filename, channels=1, sample_rate=44100, chunk_size=1024, silence_threshold=0.01, silence_duration=2):
-        self.filename = filename
-        self.channels = channels
-        self.sample_rate = sample_rate
-        self.chunk_size = chunk_size
-        self.audio_format = pyaudio.paInt16
-        self.audio_stream = None
-        self.audio_frames = []
-        self.is_recording = False
-        self.silence_threshold = silence_threshold
-        self.silence_duration = silence_duration
-        self.last_sound_time = time.time()
-
-    def start_recording(self):
-        self.is_recording = True
-        self.audio_stream = pyaudio.PyAudio().open(
-            format=self.audio_format,
-            channels=self.channels,
-            rate=self.sample_rate,
-            input=True,
-            frames_per_buffer=self.chunk_size
-        )
-
-        print("Recording started...")
-
-        threading.Thread(target=self._record).start()
-
-    def _record(self):
-        while self.is_recording:
-            data = self.audio_stream.read(self.chunk_size)
-            self.audio_frames.append(data)
-
-            # Check for silence
-            rms = self._calculate_rms(data)
-            if rms < self.silence_threshold:
-                current_time = time.time()
-                if current_time - self.last_sound_time >= self.silence_duration:
-                    self.stop_recording()
-            else:
-                self.last_sound_time = time.time()
-
-    def _calculate_rms(self, data):
-        squared_sum = sum([sample ** 2 for sample in data])
-        rms = (squared_sum / len(data)) ** 0.5
-        return rms
-
-    def stop_recording(self):
-        self.is_recording = False
-        self.audio_stream.stop_stream()
-        self.audio_stream.close()
-
-        audio = wave.open(self.filename, 'wb')
-        audio.setnchannels(self.channels)
-        audio.setsampwidth(pyaudio.PyAudio().get_sample_size(self.audio_format))
-        audio.setframerate(self.sample_rate)
-        audio.writeframes(b''.join(self.audio_frames))
-        audio.close()
-
-        print(f"Recording saved to {self.filename}")
 
 
 class Whisper(LOLLMSExtension):
